76ecdb9069f68208487315f00927e60729636a49
Antoine Kuntz <79264035+AntoineKuntz@users.noreply.github.com>
2021-05-11 15:57:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width,initial-scale=1">
         <title>Discover Mars</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.18/dist/css/uikit.min.css" />
-        <link rel="stylesheet" href="css/style.css" type="text/css">
+        <link rel="stylesheet" href="css/Indexstyle.css" type="text/css">
     </head>
     <body>
         <div class = "IndexTitle">
