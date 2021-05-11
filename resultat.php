3dbed2cc3c4e21141f3134c8efb4f89116620717
HEMMER-FX <hemmerfrancoisxavier@gmail.com>
2021-05-11 18:45:18 +0200

    ajout formulaire sans erreur sur chaque page

--- resultat.php
+++ resultat.php
@@ -1,10 +1,18 @@
 <div style="background-color:white;font-size:20px;color:red;">
     <?php
-        
-        if(($_POST['name']) === 'arc') {
+    if (isset($_POST['submit'])) {
+        $search = $_POST['name'];
+        $search = strtolower($search);
+        $search = trim($search);
+        $search = strip_tags($search);
+        $search = stripslashes($search);
+
+            
+        if($search === 'arc') {
             echo $resultat;
         }else{
             echo 'resultat inconnu';
         }
+    }
     ?>
     </div>
