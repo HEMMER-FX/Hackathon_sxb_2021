9f5ecab8d2957e9621424b637688e339d2e2d3f7
MunschSam <samuel.munsch@gmail.com>
2021-05-11 13:52:46 +0200

    header

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Title</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+       <!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.21/dist/css/uikit.min.css" />
+
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.6.21/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.6.21/dist/js/uikit-icons.min.js"></script>
+
+    </head>
+    <body>
+    
+    <nav class="uk-navbar uk-navbar-container uk-margin"  uk-offcanvas>
+    <div class="uk-navbar-left">
+        <a class="uk-navbar-toggle" href="#">
+            <button class="uk-button uk-button-default uk-margin-small-right" type="button" uk-toggle="target: #offcanvas-nav-primary"><span uk-navbar-toggle-icon></span> <span class="uk-margin-small-left">Menu</span></button>
+            <div id="offcanvas-nav-primary" uk-offcanvas="overlay: true">
+                <div class="uk-offcanvas-bar uk-flex uk-flex-column">
+
+                    <ul class="uk-nav uk-nav-primary uk-nav-center uk-margin-auto-vertical">
+                    <li><h1>Discover Mars</h1></li>
+                        <li class="uk-active"><a href="index.php">Accueil</a></li>
+                        <li class="uk-parent">
+                            <a href="gastronomie.php">Gastronomie</a>
+                            <a href="voyage.php">Voyages</a>
+                            <a href="reparation.php">Reparations</a>
+                        </li>
+                      
+                    </ul>
+
+                </div>
+            </div>
+        </a>
+    </div>
+</nav>
+ 
+        
+    
+
+
+
+
+    
+    </body>
+</html>
