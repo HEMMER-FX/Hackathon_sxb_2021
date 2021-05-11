b88af619fafb20290c6311558fbceda4bb309202
Antoine <Antoine.kuntz113@gmail.com>
2021-05-11 15:28:27 +0200

    Page d'accueil

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
+<!DOCTYPE html>
+<html>
 
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>Discover Mars</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.18/dist/css/uikit.min.css" />
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div class = "IndexTitle">
+            <h1>Discover Mars</h1>
+        </div>
+        <div class = "IndexSubTitle">
+            <h3>How Mars Can Help You Live a Better Life ...</h3>
+        </div>
+        <div class="main-contain"></div>
+
+    <div class="div">
+        <div class="flex-1">
+            <form  action="gastronomie.php" method="POST">
+            <button class="button btn-1">Gastronomie</button>
+            <form  action="voyage.php" method="POST">
+            <button class="button btn-1">Voyage</button>
+            <form  action="reparation.php" method="POST">
+            <button class="button btn-1">Vaisseau Spatial</button>
+        </div>
+    </div>
+    </h1>
+   
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.18/dist/js/uikit.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.18/dist/js/uikit-icons.min.js"></script>
+    </body>
+</html>
