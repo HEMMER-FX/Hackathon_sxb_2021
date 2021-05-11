62b303653288702336b0175aa366d7d04edf2b76
MunschSam <samuel.munsch@gmail.com>
2021-05-11 15:42:23 +0200

    repa

--- header.php
+++ header.php
@@ -4,12 +4,16 @@
         <title>Title</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css" />
        <!-- UIkit CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.21/dist/css/uikit.min.css" />
 
 <!-- UIkit JS -->
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.21/dist/js/uikit.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.21/dist/js/uikit-icons.min.js"></script>
+
+
+<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
 
     </head>
     <body>
--- reparation.php
+++ reparation.php
@@ -1,1 +1,33 @@
+<?php
+require_once "header.php";
+?>
 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Title</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <body>
+
+    <div class="uk-container uk-text-center">   
+    <h1>Reparations</h1>
+    </div>
+    <div class="uk-container2 ">   
+    <div class="switch" uk-switcher="animation: uk-animation-fade; toggle: > * ">
+    <button class="uk-button uk-button-default uk-border-rounded uk-box-shadow-hover-large " type="button"><p>Presentation<p></button>
+    <button class="uk-button uk-button-default uk-border-rounded uk-box-shadow-hover-large" type="button"><p>Prestation<p></button>
+    <button class="uk-button uk-button-default uk-border-rounded uk-box-shadow-hover-large" type="button"><p>Piece en vente<p></button>
+</div>
+
+<ul class="uk-switcher uk-margin">
+    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+    <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+    <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur, sed do eiusmod.</li>
+</ul>
+    </div>
+
+    </body>
+</html>
