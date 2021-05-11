3141bbed00dea2656bcd9eabc40b43e9ba0375c3
MunschSam <samuel.munsch@gmail.com>
2021-05-11 16:58:10 +0200

    ded

--- voyage.php
+++ voyage.php
@@ -1,1 +1,34 @@
+<?php
+require_once "header.php";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Title</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <body>
+
+    <div class="uk-container uk-text-center">   
+    <h1>Voyages</h1>
+    </div>
+    <div class="uk-container2 ">   
+    <div class="switch" uk-switcher="animation: uk-animation-fade; toggle: > * ">
+    <button class="uk-button uk-button-default uk-border-rounded uk-box-shadow-hover-large " type="button"><p>Prochain Depart<p></button>
+    <button class="uk-button uk-button-default uk-border-rounded uk-box-shadow-hover-large" type="button"><p>Destinations<p></button>
+    <button class="uk-button uk-button-default uk-border-rounded uk-box-shadow-hover-large" type="button"><p>Lieux Touristique<p></button>
+</div>
+
+<ul class="uk-switcher uk-margin">
+    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+    <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+    <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur, sed do eiusmod.</li>
+</ul>
+    </div>
+
+    </body>
+</html>
 
